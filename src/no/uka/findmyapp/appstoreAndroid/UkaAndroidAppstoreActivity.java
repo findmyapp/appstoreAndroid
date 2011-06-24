c92ab70557010d45d6809f5ca3d6bbc23eb07259
Torstein Melhus Barkve <torstein@barkve.no>
2011-06-24 10:03:54 +0200

    first commit

---
+++ src/no/uka/findmyapp/appstoreAndroid/UkaAndroidAppstoreActivity.java
@@ -0,0 +1,13 @@
+package no.uka.findmyapp.appstoreAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UkaAndroidAppstoreActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
